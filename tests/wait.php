b4102da7abb0bf49a571db27a5dbb484a222a00c
Frank de Jonge <info@frenky.net>
2017-04-12 16:09:37 +0200

    Added a wait script.

---
+++ tests/wait.php
@@ -0,0 +1,21 @@
+<?php
+
+$start = time();
+
+while (true) {
+    $response = @file_get_contents('http://username:password@localhost:15672/api/vhosts');
+
+    if ($response !== false) {
+        fwrite(STDOUT, 'Docker container started!');
+        exit(0);
+    }
+
+    $elapsed = time() - $start;
+    if ($elapsed > 30) {
+        fwrite(STDERR, 'Docker container did not start in time...' . PHP_EOL);
+        exit(1);
+    }
+
+    fwrite(STDOUT, 'Waiting for container to start...' . PHP_EOL);
+    sleep(1);
+};
